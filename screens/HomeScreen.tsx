2697a64cec5b2155534daf0cd144a21d95d00f8c
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 14:55:02 +0300

    Rokas

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import {ImageBackground, StyleSheet} from 'react-native';
-import { Text, View } from '../components/Themed';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 import { RootTabScreenProps } from '../types';
 import tw from "tailwind-react-native-classnames";
 // @ts-ignore
 import field from '../assets/images/field.jpg';
-const players = {
+const players: {[key: string]: null[]} = {
     FWD: [null, null, null],
     MID: [null, null, null],
     DEF: [null, null, null, null],
@@ -14,20 +13,20 @@ const players = {
 
 const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
     return (
-    <View style={[{backgroundColor: '#4CCF4D'},tw`flex flex-1 items-center`]}>
-        <View style={tw`mt-8`}>
-            <ImageBackground source={field} style={{width: '100%', aspectRatio: 2/3, backgroundColor: '#4CCF4D', justifyContent: 'space-around', alignItems: 'center'}} resizeMode="contain">
-                {Object.keys(players).map(position => (
-                    <View key={position} style={[{justifyContent: 'space-between'},tw`flex flex-row`]}>
-  {/*// @ts-ignore*/}
-                        {players[position].map((player) => (
-                            <Text>Player</Text>
-                        ))}
-                    </View>
-                ))}
-            </ImageBackground>
+        <View style={[{backgroundColor: '#4CCF4D'},tw`flex flex-1 items-center`]}>
+            <View style={tw`mt-8`}>
+                <ImageBackground source={field} style={{width: '100%', aspectRatio: 2/3, backgroundColor: '#4CCF4D', justifyContent: 'space-around', alignItems: 'center'}} resizeMode="contain">
+                    {Object.keys(players).map(position => (
+                        <View key={position} style={[{justifyContent: 'space-around', width: '100%'},tw`flex flex-row`]}>
+                            {/*// @ts-ignore*/}
+                            {players[position].map((player) => (
+                                <Text>Player</Text>
+                            ))}
+                        </View>
+                    ))}
+                </ImageBackground>
+            </View>
         </View>
-    </View>
   );
 }
 
