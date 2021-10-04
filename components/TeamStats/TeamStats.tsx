86db5cc2011616bcc4678a6de29febf403b76fa3
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 16:00:30 +0300

    Rokas

--- components/TeamStats/TeamStats.tsx
+++ components/TeamStats/TeamStats.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import {View, Text} from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 const TeamStats = () => {
     return (
         <View>
-            <Text>Teaam stats</Text>
+            <View style={tw``}>
+                <View style={tw``}>
+                    <Text>Players</Text>
+                </View>
+                <View style={tw``}>
+                    <Text>0/ 15</Text>
+                </View>
+            </View>
+            <View style={tw``}>
+                <View style={tw``}>
+                    <Text>Remaining</Text>
+                </View>
+                <View style={tw``}>
+                    <Text>$100m</Text>
+                </View>
+            </View>
         </View>
     );
 };
