d2a93fafaf5c65e136f9c340a81de02adf8b012c
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 14:21:15 +0300

    Rokas

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import { Text, View } from '../components/Themed';
+import { Text, View, ImageBackground } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import tw from "tailwind-react-native-classnames";
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={tw`flex flex-1 items-center justify-center`}>
-      <Text style={tw`text-lg font-bold`}>UEFA 2021</Text>
+      <ImageBackground />
     </View>
   );
 }
