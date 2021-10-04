8eb531c37fd7ffc23c37a7254943038c8d01041b
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 15:05:00 +0300

    Rokas

---
+++ components/Field/Field.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, Text, ImageBackground} from "react-native";
+import tw from "tailwind-react-native-classnames";
+// @ts-ignore
+import field from "../../assets/images/field.jpg";
+import {FontAwesome5} from "@expo/vector-icons";
+
+const players: {[key: string]: null[]} = {
+    FWD: [null, null, null],
+    MID: [null, null, null],
+    DEF: [null, null, null, null],
+    GKC: [null],
+}
+
+const Field = () => {
+    return (
+        <View style={tw``}>
+            <ImageBackground source={field} style={{width: '100%', aspectRatio: 2/3, backgroundColor: '#4CCF4D', justifyContent: 'space-around', alignItems: 'center'}} resizeMode="contain">
+                {Object.keys(players).map(position => (
+                    <View key={position} style={[{justifyContent: 'space-around', width: '100%'},tw`flex flex-row`]}>
+                        {/*// @ts-ignore*/}
+                        {players[position].map((player) => (
+                            <View key={player} style={tw`flex items-center justify-center`}>
+                                <FontAwesome5 name="tshirt" size={35} color={player ? "#d170db" : "#5c5c5cbb"} />
+                                <Text style={{backgroundColor: '#333333bb', color: 'white', fontWeight: 'bold', padding: 2, paddingHorizontal: 7, fontSize: 12}}>{position}</Text>
+                            </View>
+                        ))}
+                    </View>
+                ))}
+            </ImageBackground>
+        </View>
+    );
+};
+
+export default Field;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -5,30 +5,15 @@ import tw from "tailwind-react-native-classnames";
 // @ts-ignore
 import field from '../assets/images/field.jpg';
 import {FontAwesome5} from "@expo/vector-icons";
-const players: {[key: string]: null[]} = {
-    FWD: [null, null, null],
-    MID: [null, null, null],
-    DEF: [null, null, null, null],
-    GKC: [null],
-}
+import Field from "../components/Field/Field";
+
+
 
 const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
     return (
         <View style={[{backgroundColor: '#4CCF4D'},tw`flex flex-1 items-center`]}>
             <View style={tw`mt-8`}>
-                <ImageBackground source={field} style={{width: '100%', aspectRatio: 2/3, backgroundColor: '#4CCF4D', justifyContent: 'space-around', alignItems: 'center'}} resizeMode="contain">
-                    {Object.keys(players).map(position => (
-                        <View key={position} style={[{justifyContent: 'space-around', width: '100%'},tw`flex flex-row`]}>
-                            {/*// @ts-ignore*/}
-                            {players[position].map((player) => (
-                                <View style={tw`flex items-center justify-center`}>
-                                    <FontAwesome5 name="tshirt" size={35} color={player ? "#d170db" : "#5c5c5cbb"} />
-                                    <Text style={{backgroundColor: '#333333bb', color: 'white', fontWeight: 'bold', padding: 2, paddingHorizontal: 7, fontSize: 12}}>{position}</Text>
-                                </View>
-                            ))}
-                        </View>
-                    ))}
-                </ImageBackground>
+                <Field />
             </View>
         </View>
   );
