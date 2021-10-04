105314a22390034682a7aad752971b9f3344c9aa
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 14:24:40 +0300

    Rokas

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,11 +3,14 @@ import { StyleSheet } from 'react-native';
 import { Text, View, ImageBackground } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import tw from "tailwind-react-native-classnames";
+import field from '../assets/images/field.jpg';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={tw`flex flex-1 items-center justify-center`}>
-      <ImageBackground />
+        <ImageBackground source={field} style={{width: '100%', height: 200}}>
+
+        </ImageBackground>
     </View>
   );
 }
