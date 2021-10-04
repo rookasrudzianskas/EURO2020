49eb408a7ab6b10c1e650256aaa9d8c86f7051d3
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 17:50:40 +0300

    Rokas

--- components/PlayerListItem/PlayerListItem.tsx
+++ components/PlayerListItem/PlayerListItem.tsx
@@ -5,23 +5,25 @@ import {Feather} from "@expo/vector-icons";
 
 const PlayerListItem = () => {
     return (
-        <View style={tw``}>
+        <View style={tw`mx-6`}>
             <View>
                 <View style={tw`flex flex-row items-center`}>
-                    <Feather name="info" size={24} color="blue" />
+                    <View style={tw`p-3 border-r-2 border-gray-300`}>
+                        <Feather name="info" size={24} color="blue" />
+                    </View>
                     <Image source={{uri: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f5be112e7f395dc08ef8e58%2FLionel-Messi-celebrating-scoring-a-goal-in-the-2019-20-UEFA-Champions-League%2F1960x0.jpg%3Ffit%3Dscale'}} />
-                    <View style={tw`flex`}>
-                        <Text style={tw``}>C. Ronaldo</Text>
-                        <Text style={tw``}>GSD vs SJD</Text>
+                    <View style={tw`flex ml-2 flex-1`}>
+                        <Text style={tw` text-lg font-bold`}>C. Ronaldo</Text>
+                        <Text style={tw` text-sm`}>GSD vs SJD</Text>
                     </View>
 
                     <View style={tw`flex`}>
-                        <Text style={tw``}>$10.1</Text>
-                        <Text style={tw``}>MID</Text>
+                        <Text style={tw`text-lg font-bold`}>$10.1</Text>
+                        <Text style={tw`text-right`}>MID</Text>
                     </View>
 
                     <View style={tw`flex`}>
-                        <Text>29</Text>
+                        <Text style={tw`mx-5 text-lg font-bold`}>29</Text>
                     </View>
                 </View>
             </View>
