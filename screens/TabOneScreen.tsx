14ef52c720331c1d39bc45266c5b2b38956c763c
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 14:07:37 +0300

    Rokas

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -8,9 +8,7 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>UEFA 2021</Text>
     </View>
   );
 }
