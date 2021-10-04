69f2915805d0434c1de76ec0507da58994a48d68
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 14:40:17 +0300

    Rokas'

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -16,7 +16,7 @@ const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
   return (
     <View style={[{backgroundColor: '#4CCF4D'},tw`flex flex-1 items-center`]}>
         <View style={tw`mt-8`}>
-            <ImageBackground source={field} style={{width: '100%', aspectRatio: 2/3, backgroundColor: '#4CCF4D'}} resizeMode="contain">
+            <ImageBackground source={field} style={{width: '100%', aspectRatio: 2/3, backgroundColor: '#4CCF4D', justifyContent: 'center', alignItems: 'center'}} resizeMode="contain">
                 {Object.keys(players).map(position => (
                     <Text key={position}>{position}</Text>
                 ))}
