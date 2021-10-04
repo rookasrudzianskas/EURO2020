a7d264393121b0bc3815d0e7869d4afcb2668efb
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 15:51:24 +0300

    Rokas

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -17,19 +17,18 @@ const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
 
     return (
         <View style={[{backgroundColor: '#4CCF4D'},tw`flex flex-1 items-center`]}>
-            <View style={tw`mt-8`}>
+            <View style={tw`mt-8 flex flex-1`}>
                 <Field />
             </View>
 
-        <View style={tw`flex flex-row items-center`}>
-
+        <View style={tw`flex flex-row items-center mb-16`}>
             <TouchableOpacity activeOpacity={0.8} onPress={viewPlayers}>
                 <View style={tw`bg-yellow-500 px-24 py-4 flex flex-1 rounded-full border-2 border-yellow-600`}>
                     <Text style={tw`text-xl font-bold`}>View Players</Text>
                 </View>
             </TouchableOpacity>
 
-            <TouchableOpacity activeOpacity={0.8} onPress={viewPlayers}>
+            <TouchableOpacity activeOpacity={0.8}>
                 <View style={tw`bg-yellow-500 ml-5 p-4 rounded-full border-2 border-yellow-600`}>
                     <MaterialCommunityIcons name="fire" size={24} color="black" />
                 </View>
