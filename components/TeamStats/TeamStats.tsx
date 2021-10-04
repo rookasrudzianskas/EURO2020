f7d9e3e55e859c421319c644ab1ddbbf2f9821c7
Rokas <rookas.rudzenskas@gmail.com>
2021-10-04 16:09:20 +0300

    Rokas

--- components/TeamStats/TeamStats.tsx
+++ components/TeamStats/TeamStats.tsx
@@ -4,20 +4,20 @@ import tw from "tailwind-react-native-classnames";
 
 const TeamStats = () => {
     return (
-        <View style={[tw`flex flex-row bg-gray-100 w-full px-36 py-5`]}>
+        <View style={[tw`flex flex-row bg-gray-100 mt-4 w-full px-48 py-5 rounded-md border-4 border-blue-400`]}>
             <View style={tw`flex`}>
-                <View style={tw``}>
+                <View style={tw`-ml-36`}>
                     <Text>Players</Text>
                 </View>
-                <View style={tw``}>
+                <View style={tw`-ml-36`}>
                     <Text>0/ 15</Text>
                 </View>
             </View>
             <View style={tw`flex`}>
-                <View style={tw``}>
+                <View style={tw`-ml-20`}>
                     <Text>Remaining</Text>
                 </View>
-                <View style={tw``}>
+                <View style={tw`-ml-20`}>
                     <Text>$100m</Text>
                 </View>
             </View>
