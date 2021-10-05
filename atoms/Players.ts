05e22d05c88ed804a40325487649fb4a80e42dcb
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 10:20:49 +0300

    Rokas

--- atoms/Players.ts
+++ atoms/Players.ts
@@ -11,27 +11,33 @@ const pos2pos = {
 export const allPlayersState = selector({
     key: 'allPlayersState',
     get: async () => {
-        const response = await fetch("https://api-football-v1.p.rapidapi.com/v3/players?league=4&season=2020", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-                "x-rapidapi-key": "dea5310136msh3f59c4dcecedff3p1e01b8jsndffe742e9b8d"
-            }
-        });
+        try {
+            const response = await fetch("https://api-football-v1.p.rapidapi.com/v3/players?league=4&season=2020", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "dea5310136msh3f59c4dcecedff3p1e01b8jsndffe742e9b8d"
+                }
+            });
 
-        const json = await response.json();
+            const json = await response.json();
 
-        // @ts-ignore
-        return json.response.map((entry) => ({
-            id: entry.player.id,
-            name: entry.player.name,
-            match: 'SDA',
-            price: 12_300_000,
-        // @ts-ignore
-            position: pos2pos[entry.statistics[0].games.position],
-            totalPoints: 29,
+            // @ts-ignore
+            return json.response.map((entry) => ({
+                id: entry.player.id,
+                name: entry.player.name,
+                match: 'SDA',
+                price: 12_300_000,
+                // @ts-ignore
+                position: pos2pos[entry.statistics[0].games.position],
+                totalPoints: 29,
 
-        }))
+            }));
+        } catch (e) {
+            console.log(e);
+            return [];
+        }
+
     }
 });
 
