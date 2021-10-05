d71d715d786da694f290cd8d562b6a8f9000871a
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 08:00:28 +0300

    Rokas'

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,36 +1,31 @@
 import React from 'react';
 import {Text, TouchableOpacity, View} from "react-native";
 import tw from "tailwind-react-native-classnames";
+import {useRecoilState} from "recoil";
+import {positionFilterState} from "../../atoms/Players";
 
 
 const Filters = () => {
+
+    const [positionFilter, setPositionFilter] = useRecoilState(positionFilterState);
+    const positions = ['FWD', 'MID', 'DEF', 'GCK']
+    // console.log(positionFilter);
+    const onFilterPress = (position: string) => {
+        console.warn(position);
+    }
+
     return (
         <View style={tw`flex items-center`}>
             <View style={[{justifyContent: 'space-between'}, tw`flex flex-row items-center justify-center ml-8 mt-5`]}>
-                 <View style={[{display: 'flex', flexDirection: 'row', flex: 1},tw`flex`]}>
-                    <TouchableOpacity activeOpacity={0.8}>
-                        <Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>FWD</Text>
-                        {/*<Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>Forwards</Text>*/}
-                    </TouchableOpacity>
-                 </View>
-                 <View style={[{display: 'flex', flexDirection: 'row', flex: 1},tw`flex`]}>
-                    <TouchableOpacity activeOpacity={0.8}>
-                        <Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>MID</Text>
-                        {/*<Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>Forwards</Text>*/}
-                    </TouchableOpacity>
-                 </View>
-                 <View style={[{display: 'flex', flexDirection: 'row', flex: 1},tw`flex`]}>
-                    <TouchableOpacity activeOpacity={0.8}>
-                        <Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>DEF</Text>
-                        {/*<Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>Forwards</Text>*/}
-                    </TouchableOpacity>
-                 </View>
-                 <View style={[{display: 'flex', flexDirection: 'row', flex: 1},tw`flex`]}>
-                    <TouchableOpacity activeOpacity={0.8}>
-                         <Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>GCK</Text>
-                         {/*<Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>Forwards</Text>*/}
-                    </TouchableOpacity>
-                 </View>
+                {positions.map((position) => (
+                     <View key={position} style={[{display: 'flex', flexDirection: 'row', flex: 1},tw`flex`]}>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => onFilterPress(position)}>
+                            <Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>{position}</Text>
+                            {/*<Text style={tw`bg-gray-300 px-4 py-3 font-bold  rounded-md shadow-md`}>Forwards</Text>*/}
+                        </TouchableOpacity>
+                     </View>
+                ))}
+
             </View>
         </View>
     );
