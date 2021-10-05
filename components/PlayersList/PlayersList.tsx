13fbfac33696fb0c280547b08d5ec03ebc3689a0
Rokas <rookas.rudzenskas@gmail.com>
2021-10-05 07:47:34 +0300

    Rokas

--- components/PlayersList/PlayersList.tsx
+++ components/PlayersList/PlayersList.tsx
@@ -3,10 +3,11 @@ import {View, Text} from "react-native";
 import PlayerListItem from "../PlayerListItem/PlayerListItem";
 import {BottomSheetFlatList} from "@gorhom/bottom-sheet";
 import {allPlayersState} from "../../atoms/Players";
-import {useRecoilState} from "recoil";
+import {useRecoilState, useRecoilValue} from "recoil";
 
 const PlayersList = () => {
-    const [players] = useRecoilState(allPlayersState);
+    // const [players] = useRecoilState(allPlayersState);
+    const players = useRecoilValue(allPlayersState)
 
     // setInterval(() => setPlayers([]), 5000);
 
